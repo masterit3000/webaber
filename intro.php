eb6db3e95907022092b7964686044abce87b0f9c
masterit3000 <masterit3000@gmail.com>
2018-06-01 03:00:33 +0700

    sua intro

--- intro.php
+++ intro.php
@@ -15,23 +15,18 @@
 	<div class="container">
 		<div class="blog-post text-page" style="padding-top: 20px;">
 			<strong>ABER - DỊCH VỤ GỌI XE VỚI NHIỀU LỰA CHỌN CHO KHÁCH HÀNG</strong>
-			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Ứng dụng gọi xe ABER được ấp ủ năm 2015, bởi một nhóm
-				kỹ sư công nghệ người Việt ở Châu Âu. Tuy nhiên, đến đầu năm 2018, ABER mới triển khai thử nghiệm tại một số nước Đông
-				Nam Á. Giờ đây, ABER đã chính thức có mặt tại Việt Nam, trở thành nhà cung cấp đầu tiên về dịch vụ vận chuyển với nền
-				tảng hỗ trợ đa dạng cho nhiều loại phương tiện. Xe hai bánh chở khách, xe hai bánh giao hàng, xe bốn bánh, xe cho thuê,
-				xe tải chở hàng và xe khách đường dài.</p>
-			<p>
+			<p  class="text-justify">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Ứng dụng gọi xe ABER được ấp ủ từ năm 2015 bởi một nhóm kỹ sư công nghệ người Việt ở Châu Âu. Tuy nhiên, đến đầu năm 2018, ABER mới triển khai thử nghiệm tại một số nước Đông Nam Á. Giờ đây, ABER đã chính thức có mặt tại Việt Nam, trở thành nhà cung cấp đầu tiên về dịch vụ vận chuyển với nền tảng hỗ trợ đa dạng cho nhiều loại phương tiện: Xe hai bánh chở khách, xe hai bánh giao hàng, xe bốn bánh, xe cho thuê, xe tải chở hàng và xe khách đường dài.
 
+Dựa trên nền tảng công nghệ từ Cộng hoà liên bang Đức, đội ngũ phát triển của Aber là những người Việt trẻ ở thế hệ 8x, 9x, với những khát khao và việc xây dựng một ứng dụng kết nối nhu cầu đi lại, công tác, vận chuyển và hơn nữa là trãi nghiệm du lịch cùng ABER.
+ABER sẽ xây dựng nền tảng tốt nhất với mong muốn gia tăng trải nghiệm của khách hàng, bằng sự tận tâm và nụ cười của người dẫn đường – những tài xế ABER, với khách hàng không chỉ là di chuyển, chúng tôi gọi đó là “những chuyến xe chở niềm vui và nụ cười”.
+
+ABER Việt Nam đang hướng đến cam kết tạo nên nhiều hơn những trải nghiệm khác biệt dành cho tài xế và hành khách, đồng thời đối với ABER, đó còn là trách nhiệm đối với đội ngũ tài xế…
+</p>
+			<strong>ABER là AM BESTEN FAHRER hoặc ABSOLUTE DRIVER: “Người tài xế hoàn hảo”</strong>
+			<p class="text-justify">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Bắt đầu từ 01/06/2018, ABER đã chính thức tuyển dụng tài xế tại thành phố Hồ Chí Minh, sau đó phát triển ra Hà Nội và một số tỉnh, thành khác.</p>
+			<p  class="text-justify">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Với mục tiêu kết nối các hạ tầng và dịch vụ giao thông, ABER cung cấp cho người dùng tính năng đặt đa dạng các phương tiện giao thông, đặc biệt với các tuyến xe đường dài, xe cho thuê, xe tải - nhằm đáp ứng nhu cầu ngày một cao của người dùng.
 			</p>
-			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Ông Nguyễn Bình - Nhà sáng lập ABER cho biết: Hiện
-				ABER đang hoàn tất các khâu chuẩn bị cuối cùng để cho ra mắt sàn giao dịch gọi xe lần đầu tiên có ở Việt Nam với nền
-				tảng hỗ trợ đa dạng cho nhiều loại phương tiện. Dự kiến, từ 01/06/2018 ABER sẽ bắt đầu tuyển mộ tài xế tại thành phố
-				Hồ Chí Minh, sau đó phát triển ra Hà Nội và một số tỉnh, thành khác.</p>
-			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Với mục tiêu kết nối các hạ tầng và dịch vụ giao thông,
-				ABER cung cấp cho người dùng khả năng đặt đa dạng các phương tiện giao thông, đặc biệt với các tuyến xe đường dài, xe
-				cho thuê, xe tải - nhằm đáp ứng nhu cầu ngày một cao của người dùng.
-			</p>
-			<strong>Khách hàng được tiếp cận với:</strong>
+			<strong>Khách hàng tiếp cận được dịch vụ dễ dàng và tiện lợi hơn với:</strong>
 			<ol>
 				<li> - Ứng dụng gốc cho iOS & Android </i>
 					<li> - Giao thức kết nối bảo mật tiêu chuẩn </li>
@@ -44,7 +39,11 @@
 					<li> - Kết hợp dịch vụ hoàn hảo/ nhiều lựa chọn..</li>
 
 			</ol>
-			<strong> ABER - Nền tảng hỗ trợ đa dạng cho nhiều loại phương tiện</strong>
+			<p  class="text-justify">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ABER là giải pháp chung giúp khai thác thế mạnh của công nghệ và đưa vào đó những yếu tố thay đổi và phù hợp với thị trường nhằm thúc đẩy sự phát triển của ngành giao thông Việt.
+			</p>
+			<p  class="text-justify">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mục tiêu của chúng tôi là xây dựng một nền tảng có khả năng kết nối các dịch vụ giao thông và nhu cầu của khách hàng một cách tiện lợi, công bằng và hợp lý.
+</p>
+<p class="text-right">ĐỘI NGŨ PHÁT TRIỂN ABER</p>
 		</div>
 	</div>
 	
