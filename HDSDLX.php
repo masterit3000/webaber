dd71b1e08e39216f3a834fc79b4c1cd7ab3489cd
unknown <vukhoi1999>
2018-06-01 00:34:50 +0700

    em sua trang HDSDLX

--- HDSDLX.php
+++ HDSDLX.php
@@ -1,73 +1,153 @@
-<header class="page-header" style="background-image: url('public/images/_inner-bg.jpg');">
+<section id="services">
 		<div class="container">
-			<ol class="bread">
-				<li>
-					<a href="index.html"><span>Trang chủ</span></a>
-				</li>
-				<li class="divider"><span>//</span></li>
-				<li>
-					<span>HƯỚNG DẪN SỬ DỤNG CHO LÁI XE</span>
-				</li>
-			</ol>		
-			<h1>HƯỚNG DẪN SỬ DỤNG CHO LÁI XE</h1>
-		</div>
-	</header>
-	<div class="container">
-		<div class="blog-post text-page" style="padding-top: 20px;">
-			<strong>HƯỚNG DẪN SỬ DỤNG CHO LÁI XE</strong>
-			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Ứng dụng gọi xe ABER được ấp ủ năm 2015, bởi một nhóm
-				kỹ sư công nghệ người Việt ở Châu Âu. Tuy nhiên, đến đầu năm 2018, ABER mới triển khai thử nghiệm tại một số nước Đông
-				Nam Á. Giờ đây, ABER đã chính thức có mặt tại Việt Nam, trở thành nhà cung cấp đầu tiên về dịch vụ vận chuyển với nền
-				tảng hỗ trợ đa dạng cho nhiều loại phương tiện. Xe hai bánh chở khách, xe hai bánh giao hàng, xe bốn bánh, xe cho thuê,
-				xe tải chở hàng và xe khách đường dài.</p>
-			<p>
-
-			</p>
-			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Ông Nguyễn Bình - Nhà sáng lập ABER cho biết: Hiện
-				ABER đang hoàn tất các khâu chuẩn bị cuối cùng để cho ra mắt sàn giao dịch gọi xe lần đầu tiên có ở Việt Nam với nền
-				tảng hỗ trợ đa dạng cho nhiều loại phương tiện. Dự kiến, từ 01/06/2018 ABER sẽ bắt đầu tuyển mộ tài xế tại thành phố
-				Hồ Chí Minh, sau đó phát triển ra Hà Nội và một số tỉnh, thành khác.</p>
-			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Với mục tiêu kết nối các hạ tầng và dịch vụ giao thông,
-				ABER cung cấp cho người dùng khả năng đặt đa dạng các phương tiện giao thông, đặc biệt với các tuyến xe đường dài, xe
-				cho thuê, xe tải - nhằm đáp ứng nhu cầu ngày một cao của người dùng.
-			</p>
-			<strong>Khách hàng được tiếp cận với:</strong>
-			<ol>
-				<li> - Ứng dụng gốc cho iOS & Android </i>
-					<li> - Giao thức kết nối bảo mật tiêu chuẩn </li>
-					<li> - Lựa chọn điểm xuống - tự điều chỉnh tọa độ trên bản đồ hoặc nhập địa chỉ </li>
-					<li> - Theo dõi vị trí của xe trên bản đồ</li>
-					<li> - Đặt xe đơn giản và tiện lợi / hỗ trợ đặt trước </li>
-					<li> - Liên hệ với tài xế Tự động tính giá cước - không lo sai lệch giá </li>
-					<li> - Nhiều lựa chọn thanh toán - tiền mặt / thẻ / ví điện tử </li>
-					<li> - Hủy đặt xe Thông báo - Push Notification trên ứng dụng, email, sms Xem lịch sử giao dịch </li>
-					<li> - Kết hợp dịch vụ hoàn hảo/ nhiều lựa chọn..</li>
-
-			</ol>
-			<strong> ABER - Nền tảng hỗ trợ đa dạng cho nhiều loại phương tiện</strong>
-		</div>
-	</div>
-	<section id="partners">
-		<div class="container">
+			<!-- <h4 class="yellow">Chào mừng</h4> -->
+			<h2 class="h2">Quy trình đăng ký lái xe</h2>
 			<div class="row">
-				<div class="col-md-3 col-sm-12">
-					<h4 class="white margin-0">Đối tác</h4>
-					<h2 class="white margin-0">và khách hàng</h2>
-				</div>
-				<div class="col-md-9 col-sm-12">
-					<div class="row items">
-						<div class="col-md-5ths col-sm-3 col-ms-4 col-xs-6">
-							<a href="http://megaview.vn/" target="_blank">
-								<img src="public/images/logo.png" alt="Partner" style="max-width: 95%">
-							</a>
-						</div>
-						<div class="col-md-5ths col-sm-3 col-ms-4 col-xs-6">
-							<a href="http://hyundai.com/" target="_blank">
-								<img src="public/images/huyndai.png" alt="Partner" height="70px">
-							</a>
-						</div>
+				<div class="col-md-4 col-sm-6 col-ms-6 matchHeight">
+					<div class="image">
+						<img src="public/images/Picture1.png" alt="Service">
 					</div>
+					<h5>No1. Chuẩn bị hồ sơ tham gia Aber</h5>
+				</div>
+				<div class="col-md-4 col-sm-6 col-ms-6 matchHeight">
+					<div class="image">
+						<img src="public/images/Picture2.png" alt="Service">
+					</div>
+					<h5>No2. Phỏng vấn/tuyển chọn</h5>
+				</div>
+				<div class="col-md-4 col-sm-6 col-ms-6 matchHeight">
+					<div class="image">
+						<img src="public/images/Picture4-1.png" alt="Service">
+					</div>
+					<h5>No.3 Cập nhật hồ sơ đối tác
+Đào tạo – chụp hình </h5>
 				</div>
 			</div>
 		</div>
-	</section>
+    </section>
+    <section id="services">
+		<div class="container">
+			<!-- <h4 class="yellow">Chào mừng</h4> -->
+			
+			<div class="row">
+				<div class="col-md-4 col-sm-6 col-ms-6 matchHeight">
+					<div class="image">
+                    <img src="public/images/Picture5.png" style="width:330px;Height:180px" alt="Service">
+					</div>
+					<h5>No4. Nạp tiền lần đầu
+Kích hoạt tài khoản</h5>
+				</div>
+				<div class="col-md-4 col-sm-6 col-ms-6 matchHeight">
+					<div class="image">
+						<img src="public/images/Picture6.png" alt="Service">
+					</div>
+                    <h5>No5. Đồng phục</h5>
+                    <p>(Không bắt buộc)</p>
+				</div>
+				<div class="col-md-4 col-sm-6 col-ms-6 matchHeight">
+					<div class="image">
+						<img src="public/images/Picture7-1.png" alt="Service">
+					</div>
+                    <h5>No6. Giới thiệu về tổ đội/nhóm </h5>
+                    <p>Theo dõi và hỗ trợ đối tác trong công việc</p>
+				</div>
+			</div>
+		</div>
+    </section>
+    <section id="services">
+    <div class="container">
+        <h2 class="h2">Thủ tục tham gia Aber Việt Nam</h2>
+        <div class="row">
+            <div class="col-md-6 col-sm-6 col-ms-6 matchHeight">
+                <div class="image">
+                    <img src="public/images/_car-big-2.png" Height="380px" width="500px" alt="Service">
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6 col-ms-6 matchHeight text-justify">
+                <p>Để tham gia và trở thành đối tác Aber, người hợp tác cần đáp ứng đầy đủ các điều kiện và thủ tục sau:
+                    Là Công dân Việt Nam từ 18 tuổi đến 65 tuổi, không bị mất năng lực hành vi dân sự hoặc hạn chế năng lực hành vi nhân sự; đủ điều kiện sức khoẻ để điều khiển xe mô tô theo quy định.
+                    Có quyền sở hữu hoặc quyền quản lý và sử dụng xe mô tô 2 bánh có đăng ký và gắn biển số do cơ quan Nhà nước có thẩm quyền cấp, và phải đảm bảo các quy định về chất lượng, an toàn kỹ thuật và bảo vệ môi trường như sau:
+                    Có đủ hệ thống hãm (phanh/thắng) có hiệu lực;
+                    Có hệ thống chuyển hướng (hệ thống lái) có hiệu lực;
+                    Có đủ đèn chiếu sáng gần và xa, đèn soi biển số, đèn báo hãm, đèn tín hiệu;
+                    Có bánh lốp đúng kích cỡ và đúng tiêu chuẩn kỹ thuật của từng loại xe;
+                    Có gương chiếu hậu và các trang bị, thiết bị khác bảo đảm tầm nhìn cho người điều khiển;
+                    Có còi với âm lượng đúng quy chuẩn kỹ thuật;
+                    Có đủ bộ phận giảm thanh, giảm khói và các trang bị, thiết bị khác bảo đảm khí thải, tiếng ồn theo quy chuẩn môi trường;
+                    Các kết cấu phải đủ độ bền và bảo đảm tính năng vận hành ổn định.
+                    Có điện thoại thông minh chạy hệ điều hành Android từ 4.4 trở lên hoặc hệ điều hành iOS từ 10.0 trở lên và có sim 3G/4G.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="services">
+    <div class="container">
+        <h2 class="h2">Các thủ tục dành cho đối tác - Moto</h2>
+        <div class="row">
+            <div class="col-md-6 col-sm-6 col-ms-6 matchHeight">
+					<div class="image">
+                    <img src="public/images/Picture9.jpg" style="width:330px;Height:180px" alt="Service">
+					</div>
+					<h5 class="text-center" >ĐỐI TÁC LÀ SINH VIÊN</h5>
+                    <p class="text-justify">1/ CMND/CCCD/Hộ chiếu: Còn hạn sử dụng.</br>
+                                            2/ Thẻ sinh viên: Còn giá trị sử dụng.</br>
+                                            3/ Giấy đăng ký xe (cà vẹt xe) – không cần chính chủ.</br>
+                                            4/ Giấy phép lái xe (hạng A1, A2): Còn hạn sử dụng.</br>
+                                            5/ Bảo hiểm xe: Còn hạn sử dụng, đúng tên với cà vẹt xe máy tham gia Aber.</br>
+                    </p>
+            </div>
+            <div class="col-md-6 col-sm-6 col-ms-6 matchHeight">
+					<div class="image">
+                    <img src="public/images/Picture10.png" style="width:330px;Height:180px" alt="Service">
+					</div>
+					<h5 class="text-center" >ĐỐI TÁC CÁ NHÂN</h5>
+                    <p class="text-justify">1/ CMND/CCCD/Hộ chiếu: Còn hạn  sử dụng.</br>
+                                            2/Hộ khẩu thường trú/KT3.</br>
+                                            3/ Sơ yếu lý lịch địa phương/Xác nhận hạnh kiểm/Lý lịch tư pháp: Có chứng nhận của UBND cấp có thẩm quyền nơi cư trú (thời hạn không quá 6 tháng. Nếu thiếu bổ sung trong vòng 15 ngày).</br>
+                                            4/ Giấy đăng ký xe (cà vẹt xe) – không cần chính chủ.</br>
+                                            5/ Giấy phép lái xe (hạng A1, A2): Còn hạn sử dụng.</br>
+                                            6/ Bảo hiểm xe: Còn hạn sử dụng, đúng tên với cà vẹt xe máy tham gia Aber.</br>
+                    </p>
+			</div>
+        </div>
+    </div>
+</section>
+<section id="services">
+    <div class="container">
+        <h2 class="h2">Các thủ tục dành cho đối tác - Ôto</h2>
+        <div class="row">
+            <div class="col-md-6 col-sm-6 col-ms-6 matchHeight">
+					<div class="image">
+                    <img src="public/images/Picture10.png" style="width:330px;Height:180px" alt="Service">
+					</div>
+					<h5 class="text-center" >ĐỐI TÁC CÁ NHÂN</h5>
+                    <p class="text-justify">1/ CMND/CCCD/Hộ chiếu: Còn hạn  sử dụng.</br>
+                                            2/Hộ khẩu thường trú/KT3.</br>
+                                            3/ Sơ yếu lý lịch địa phương/Xác nhận hạnh kiểm/Lý lịch tư pháp: Có chứng nhận của UBND cấp có thẩm quyền nơi cư trú (thời hạn không quá 6 tháng. Nếu thiếu bổ sung trong vòng 15 ngày).</br>
+                                            4/ Giấy khám sức khỏe có xét nghiệm ma túy.</br>
+                                            5/ Giấy đăng ký xe (cà vẹt xe) – Giấy chứng nhận đăng kiểm kinh doanh.</br>
+                                            6/ Giấy phép lái xe (hạng B1 trở lên): Còn hạn sử dụng.</br>
+                                            7/ Bảo hiểm xe bắt buộc trách nhiệm dân sự kinh doanh: Còn hạn sử dụng.</br>
+                                            8/ Giấy xác nhận đăng ký tham gia HTX, nếu có.</br>
+                                            9/ Phù hiệu xe chạy hợp đồng.</br>
+                    </p>
+            </div>
+            <div class="col-md-6 col-sm-6 col-ms-6 matchHeight">
+					<div class="image">
+                    <img src="public/images/Picture11.png" style="width:330px;Height:180px" alt="Service">
+					</div>
+					<h5 class="text-center" >ĐỐI TÁC DOANH NGHIỆP</h5>
+                    <p class="text-justify">1/ CMND/CCCD/Hộ chiếu chủ doanh nghiệp: Còn hạn  sử dụng.</br>
+                                            2/ Giấy phép đăng ký kinh doanh doanh nghiệp: Còn hiệu lực</br>
+                                            3/ Giấy đăng ký và chứng nhận kinh doanh ngành vận tải, xe hợp đồng, xe du lịch, và các gaiasy tờ liên quan.</br>
+                                            4/ Danh sách thành viên, nhân viên công ty tham gia.</br>
+                                            5/ Danh sách oto trực thuộc doanh nghiệp đang ký chạy Aber.</br>
+                                            6/ Giấy phép lái xe (hạng B2 trở lên) của các nhân viên: Còn hạn sử dụng.</br>
+                                            7/ Bảo hiểm xe bắt buộc trách nhiệm dân sự kinh doanh: Còn hạn sử dụng.</br>
+                                            8/ Phù hiệu xe chạy hợp đồng của công ty được Sở GTVT cấp: Còn hiệu lực.</br>
+                    </p>
+			</div>
+        </div>
+    </div>
+</section>
