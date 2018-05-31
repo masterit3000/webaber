fbd1ae5ee1e340aaf788acd82b48145ceac52825
unknown <vukhoi1999>
2018-05-31 22:20:22 +0700

    sua dich vu va sẻvices

--- dichvu.php
+++ dichvu.php
@@ -1,4 +1,4 @@
-<section id="services">
+<section id="dichvu">
     <div class="container">
         <!-- <h4 class="yellow">Chào mừng</h4> -->
         <h2 class="h1">Dịch vụ của chúng tôi</h2>
@@ -6,31 +6,30 @@
         <ul class="row nav nav-tabs">
             <li class="active col-md-4 col-sm-6 col-ms-6 matchHeight">
                 <a data-toggle="tab" href="#tab1">
-                    <div class="image">
+                    <div class="image text-center" >
                         <img src="public/images/Picture7.png" width="150px" height="150px" alt="Service">
                     </div>
-                    <h5>ABER BIKE</h5>
-                    <p>Những chiếc xe máy thân thuộc cùng bạn đồng hành trên mọi nẽo đường với cả trái tim và những nụ cười</p>
+                    <h5 class="text-center" >ABER BIKE</h5>
+                    <p class="text-justify">Những chiếc xe máy thân thuộc cùng bạn đồng hành trên mọi nẽo đường với cả trái tim và những nụ cười</p>
                 </a>
             </li>
             <li class=" col-md-4 col-sm-6 col-ms-6 matchHeight">
                 <a data-toggle="tab" href="#tab2">
-                    <div class="image">
+                    <div class="image text-center">
                         <img src="public/images/Picture3.png" width="150px" height="150px" alt="Service">
                     </div>
-                    <h5>ABER CAR
-                    </h5>
-                    <p>Phong cách, sang trọng, lĩnh lãm, thoải mái và an toàn, Aber Car cam kết sẽ đem đến cho bạn dịch vụ tốt nhất</p>
+                    <h5 class="text-center">ABER CAR</h5>
+                    <p class="text-justify">Phong cách, sang trọng, lĩnh lãm, thoải mái và an toàn, Aber Car cam kết sẽ đem đến cho bạn dịch vụ tốt nhất</p>
 
                 </a>
             </li>
             <li class=" col-md-4 col-sm-6 col-ms-6 matchHeight">
                 <a data-toggle="tab" href="#tab3">
-                    <div class="image">
+                    <div class="image text-center">
                         <img src="public/images/Picture8.png" width="150px" height="150px" alt="Service">
                     </div>
-                    <h5>ABER TRUCK </h5>
-                    <p>Uy tín, chất lượng, tiện lợi và nhanh chóng, là những tiện ích mà Aber Truck sẽ đem đến cho bạn
+                    <h5 class="text-center">ABER TRUCK </h5>
+                    <p class="text-justify">Uy tín, chất lượng, tiện lợi và nhanh chóng, là những tiện ích mà Aber Truck sẽ đem đến cho bạn
                     </p>
                 </a>
             </li>
@@ -39,77 +38,77 @@
 
 
 
-        <div class="tab-content" style="margin-top: 50px">
+        <div class="tab-content" style="margin-top: 30px">
             <div id="tab1" class="tab-pane fade in active row">
                 <div class="col-md-4 col-sm-6 col-ms-6 ">
-<!--                    <div class="image">
-                        <img src="public/images/Picture7.png" width="150px" height="150px" alt="Service">
-                    </div>-->
-                    <h5>ABER BIKE</h5>
-                    <p>Xe ôm công nghệ</p>
-                    <p>Xe ôm là từ ngữ quen thuộc mà mọi người gọi cánh tài xế sử dụng xe máy cho những chuyến đi của khách hàng. Và, với trách nhiệm trách nhiệm đưa đón khách hàng, các tài xế Aber xe ôm đều am hiểu đường phố và luôn lái xe với cả trái tim cũng những nụ cười
-</p>
+                    <!--                    <div class="image">
+                                            <img src="public/images/Picture7.png" width="150px" height="150px" alt="Service">
+                                        </div>-->
+                    <h5 class="text-center">ABER BIKE</h5>
+                    <p class="text-center">Xe ôm công nghệ</p>
+                    <p class="text-justify">Xe ôm là từ ngữ quen thuộc mà mọi người gọi cánh tài xế sử dụng xe máy cho những chuyến đi của khách hàng. Và, với trách nhiệm trách nhiệm đưa đón khách hàng, các tài xế Aber xe ôm đều am hiểu đường phố và luôn lái xe với cả trái tim cũng những nụ cười
+                    </p>
                 </div>
                 <div class="col-md-4 col-sm-6 col-ms-6 ">
-<!--                    <div class="image">
-                        <img src="public/images/Picture3.png" width="150px" height="150px" alt="Service">
-                    </div>-->
-                    <h5>ABER TRAVEL </h5>
-                    <p>Trải nghiệm du lịch cùng Aber</p>
-                    <p>Aber với sự am hiểu về những tuyến đường và kiến thức văn hóa vùng miền, chúng tôi sẽ đem đến cho những khách hàng những trải nghiệm khó quên về những tour du lịch trải nghiệm đường phố Sài Gòn theo những góc nhìn từ Aber.
-Aber Travel là sự phát triển và mở rộng từ Aber Bike với chi phí dịch vụ cạnh tranh và linh hoạt</p>
+                    <!--                    <div class="image">
+                                            <img src="public/images/Picture3.png" width="150px" height="150px" alt="Service">
+                                        </div>-->
+                    <h5 class="text-center">ABER TRAVEL </h5>
+                    <p class="text-center">Trải nghiệm du lịch cùng Aber</p>
+                    <p class="text-justify">Aber với sự am hiểu về những tuyến đường và kiến thức văn hóa vùng miền, chúng tôi sẽ đem đến cho những khách hàng những trải nghiệm khó quên về những tour du lịch trải nghiệm đường phố Sài Gòn theo những góc nhìn từ Aber.
+                        Aber Travel là sự phát triển và mở rộng từ Aber Bike với chi phí dịch vụ cạnh tranh và linh hoạt</p>
                 </div>
                 <div class="col-md-4 col-sm-6 col-ms-6 ">
-<!--                    <div class="image">
-                        <img src="public/images/Picture8.png" width="150px" height="150px" alt="Service">
-                    </div>-->
-                    <h5>Aber Express </h5>
-                    <p>Xe giao hàng</p>
-                    <p>Nhanh chóng và tiện lợi, Aber Express giúp bạn giao nhận hàng hóa đến bất địa điểm nào trong thành phố, với tiêu chí: Nhanh chóng, hiện đại, chuyên nghiệp và uy tin </p>
+                    <!--                    <div class="image">
+                                            <img src="public/images/Picture8.png" width="150px" height="150px" alt="Service">
+                                        </div>-->
+                    <h5 class="text-center">Aber Express </h5>
+                    <p class="text-center"> Xe giao hàng</p>
+                    <p class="text-justify">Nhanh chóng và tiện lợi, Aber Express giúp bạn giao nhận hàng hóa đến bất địa điểm nào trong thành phố, với tiêu chí: Nhanh chóng, hiện đại, chuyên nghiệp và uy tin </p>
                 </div>
             </div>
-            
-            
-            
-            
+
+
+
+
             <div id="tab2" class="tab-pane fade row">
                 <div class="col-md-4 col-sm-6 col-ms-6 matchHeight">
-<!--                    <div class="image">
-                        <img src="public/images/Picture7.png" width="150px" height="150px" alt="Service">
-                    </div>-->
-                    <h5>Aber Car</h5>
-                    <p>Là việc các đối tác Aber có oto và sử dụng app Aber cho việc phục vụ nhu cầu của khách hàng. Khách hàng dễ dàng đặt xe qua app Aber cho nhu cầu di chuyển đa dạng, an toàn và thoải mái</p>
+                    <!--                    <div class="image">
+                                            <img src="public/images/Picture7.png" width="150px" height="150px" alt="Service">
+                                        </div>-->
+                    <h5 class="text-center">Aber Car</h5>
+                    <p class="text-justify">Là việc các đối tác Aber có oto và sử dụng app Aber cho việc phục vụ nhu cầu của khách hàng. Khách hàng dễ dàng đặt xe qua app Aber cho nhu cầu di chuyển đa dạng, an toàn và thoải mái</p>
                 </div>
                 <div class="col-md-4 col-sm-6 col-ms-6 matchHeight">
-<!--                    <div class="image">
-                        <img src="public/images/Picture3.png" width="150px" height="150px" alt="Service">
-                    </div>-->
-                    <h5>Aber - Xe cho thuê</h5>
-                    <p>Nhằm phụ vụ nhu cầu đường dài và kết hợp du lịch. Aber có những dòng xe chuyên dụng nhằm phục vụ và đáp ứng nhu cầu đi lại của khách hàng
-</p>
+                    <!--                    <div class="image">
+                                            <img src="public/images/Picture3.png" width="150px" height="150px" alt="Service">
+                                        </div>-->
+                    <h5 class="text-center">Aber - Xe cho thuê</h5>
+                    <p class="text-justify">Nhằm phụ vụ nhu cầu đường dài và kết hợp du lịch. Aber có những dòng xe chuyên dụng nhằm phục vụ và đáp ứng nhu cầu đi lại của khách hàng
+                    </p>
                 </div>
                 <div class="col-md-4 col-sm-6 col-ms-6 matchHeight">
-<!--                    <div class="image">
-                        <img src="public/images/Picture8.png" width="150px" height="150px" alt="Service">
-                    </div>-->
-                    <h5>AAber - Xe doanh nghiệp</h5>
-                    <p>Trong nền kinh tế phát triển, nhu cầu đi lại và gặp gỡ khách hàng là nhu cầu thiết yếu của các doanh nghiệp. Aber đem đến giải pháp Aber Business, với đội ngũ được đào tạo chuyên nghiệp</p>
+                    <!--                    <div class="image">
+                                            <img src="public/images/Picture8.png" width="150px" height="150px" alt="Service">
+                                        </div>-->
+                    <h5 class="text-center">AAber - Xe doanh nghiệp</h5>
+                    <p class="text-justify">Trong nền kinh tế phát triển, nhu cầu đi lại và gặp gỡ khách hàng là nhu cầu thiết yếu của các doanh nghiệp. Aber đem đến giải pháp Aber Business, với đội ngũ được đào tạo chuyên nghiệp</p>
                 </div>
             </div>
-            
-            
-            
+
+
+
             <div id="tab3" class="tab-pane fade row">
                 <div class="col-md-12 col-sm-12 col-ms-12 matchHeight ">
-<!--                    <div class="image">
-                        <img src="public/images/Picture7.png" width="150px" height="150px" alt="Service">
-                    </div>-->
-                    <h5>Aber Truck</h5>
-                    <p>Xe giao hàng – Xe tải</p>
-                    <p>Lần đầu tiên xe tải chở hàng được đặt qua ứng dụng công nghệ. Aber là công ty về ứng sụng công đã đi tiên phong trong vấn đề này. Chỉ cần 1 vài thao tác đơn giản trên app Aber, là bạn sẽ có được đội ngũ chuyên nghiệp trong lĩnh vực vận tãi sẽ đem đến cho khách sự thoải mái, tiện lợi và uy tín trong từng chuyến hàng và dịch vụ. Và bạn có thể cho các yêu cầu về dịch vụ, để Aber phục vụ bạn được tốt hơn</p>
+                    <!--                    <div class="image">
+                                            <img src="public/images/Picture7.png" width="150px" height="150px" alt="Service">
+                                        </div>-->
+                    <h5 class="text-center">Aber Truck</h5>
+                    <p class="text-center">Xe giao hàng – Xe tải</p>
+                    <p class="text-justify">Lần đầu tiên xe tải chở hàng được đặt qua ứng dụng công nghệ. Aber là công ty về ứng sụng công đã đi tiên phong trong vấn đề này. Chỉ cần 1 vài thao tác đơn giản trên app Aber, là bạn sẽ có được đội ngũ chuyên nghiệp trong lĩnh vực vận tãi sẽ đem đến cho khách sự thoải mái, tiện lợi và uy tín trong từng chuyến hàng và dịch vụ. Và bạn có thể cho các yêu cầu về dịch vụ, để Aber phục vụ bạn được tốt hơn</p>
                 </div>
-               
-                
+
+
             </div>
         </div>
         <!--        <div class="row">
--- services.php
+++ services.php
@@ -13,8 +13,31 @@
     </div>
 </header>
 
-<?php 
-    include './dichvu.php';
+<section id="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2 col-sm-6 col-ms-6 matchHeight">
+                <div class="image">
+                    <img src="public/images/Picture8.png" width="150px" height="150px" alt="Service">
+                </div>
+            </div>
+            <div class="col-md-10 col-sm-6 col-ms-6 matchHeight text-justify">
+                <h5>TẠI SAO BẠN NÊN CHỌN ABER </h5>
+                <p>Giá cước cạnh tranh Chi phí xe hiển thị ngay trên ứng dụng, bạn chỉ việc trả đúng số tiền này và an tâm tận hưởng chuyến đi. Chúng tôi luôn cố gắng để hài hoà lợi ích giữa tài xế và hành khách, với tài xế, chúng tôi tham khảo và đưa ra con số chiết khấu hấp dẫn nhất, vì chúng tôi biết, sau lưng bạn là cả một gia đình.
+
+                    Với hành khách, chúng tôi nỗ lực để bạn có thêm nhiều ưu đãi nhất cho mỗi chuyến đi, An toàn là trên hết Không những với tài xế trên suốt hành trình, Aber còn có bảo hiểm dành cho hành khách vì sự an toàn cho bạn và tài xế trong chuyến đi, như mỗi thành viên trong gia đình Aber
+
+                    Các tài xế Aber đều có kinh nghiệm lái xe lâu năm, được hướng dẫn chu đáo để mang lại một dịch vụ tốt và an toàn nhất cho bạn. Ngoài ra bạn hoàn toàn có thể chọn hình thức & chât lượng xe theo mong muốn với phi phí tối ưu nhất
+
+                    Đến với Aber, khách hàng yên tâm về giá vì chúng tôi cam kết không tăng giá trong giờ cao điểm
+                </p>
+            </div>
+        </div>
+    </div>
+
+</section>
+<?php
+include './dichvu.php';
 ?>
 
 <section id="testimonials">
@@ -111,6 +134,6 @@
         </div>
     </div>
 </section>
-<?php 
+<?php
 include_once './doitac.php';
 ?>
